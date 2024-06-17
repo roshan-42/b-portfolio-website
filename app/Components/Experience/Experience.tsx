9e524530b32389aafa65a947d0f6d425255d4663
roshan-42 <roshanacharya207@gmail.com>
2024-06-17 18:34:32 +0545

    anim added

--- app/Components/Experience/Experience.tsx
+++ app/Components/Experience/Experience.tsx
@@ -5,17 +5,17 @@ import { LuBookMinus } from 'react-icons/lu'
 const Experience = () => {
     return (
         <motion.div
-        initial={{ opacity: 0, translateY: 50 }}
-        whileInView={{ opacity: 1, translateY: 0 }}
-        viewport={{ once: false }}
-        transition={{ duration: 1 }} className='flex items-center flex-col justify-center text-gray-600 p-4 gap-5'>
+            className='flex items-center flex-col justify-center text-gray-600 p-4 gap-5'>
             <div className='flex flex-col items-center '>
                 <h1>Experience</h1>
                 <h2 className='text-5xl text-black'>Professional Resume</h2>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 border gap-4  w-full place-items-center '>
                 {/* first */}
-                <div className='flex flex-col gap-10 border border-gray-300 p-10 h-80 rounded-xl '>
+                <motion.div initial={{ opacity: 0, translateY: 50 }}
+                    whileInView={{ opacity: 1, translateY: 0 }}
+                    viewport={{ once: false }}
+                    transition={{ duration: 1 }} className='flex flex-col gap-10 border border-gray-300 p-10 h-80 rounded-xl '>
                     <div className='flex items-center gap-5   '>
                         <div className='bg-white p-3 rounded-xl'>
                             <LuBookMinus size={40} />
@@ -42,9 +42,12 @@ const Experience = () => {
                         </div>
 
                     </div>
-                </div>
+                </motion.div>
                 {/* second */}
-                <div className='flex flex-col gap-10 border border-gray-300 p-10  h-80  rounded-xl'>
+                <motion.div initial={{ opacity: 0, translateY: 50 }}
+                    whileInView={{ opacity: 1, translateY: 0 }}
+                    viewport={{ once: false }}
+                    transition={{ delay: 0.1, duration: 1 }} className='flex flex-col gap-10 border border-gray-300 p-10  h-80  rounded-xl'>
                     <div className='flex items-center gap-5   '>
                         <div className='bg-white p-3 rounded-xl'>
                             <LuBookMinus size={40} />
@@ -72,7 +75,7 @@ const Experience = () => {
                         </div>
 
                     </div>
-                </div>
+                </motion.div>
 
                 <div>
 
